9ba3d3e16ea70ba6bac07f0f65fd4b49417a3d5d
seherpuri <seher@puri.org>
2024-09-15 22:28:55 -0400

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="menu-icon" id="hamburger-icon" onclick="toggleMenu()">
+        &#9776;
+    </div>
+    <nav id="navbar" class="nav-menu">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Showcase</a></li>
+        </ul>
+    </nav>
+</header>
